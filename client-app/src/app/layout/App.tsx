7b914be509cad4c9dc58a0d786c5a89ccc8630e7
Rodney Wormsbecher <rodney22@live.nl>
2019-10-23 22:39:37 +0200

    added basic scaffold of the UI

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Header, Icon, List } from "semantic-ui-react";
+import { Header, Icon, List, Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([]);
@@ -13,17 +15,14 @@ const App = () => {
     }, []);
 
     return (
-        <div>
-            <Header as="h2">
-                <Icon name="users" />
-                <Header.Content>Reactivities</Header.Content>
-            </Header>
+        <React.Fragment>
+            <NavBar />
             <List>
-                {activities.map(activity => (
-                    <List.Item key={activity.id}>{activity.title}</List.Item>
-                ))}
+                <Container style={{ marginTop: "7em" }}>
+                    <ActivityDashboard activities={activities}></ActivityDashboard>
+                </Container>
             </List>
-        </div>
+        </React.Fragment>
     );
 };
 

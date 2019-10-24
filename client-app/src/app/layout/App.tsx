dff76b6905acba0bba42f8297f605da47114894e
Rodney Wormsbecher <rodney22@live.nl>
2019-10-25 00:56:04 +0200

    created layout

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Header, Icon, List, Container } from "semantic-ui-react";
+import { List, Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
@@ -9,25 +9,61 @@ const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
 
+    const [editMode, setEditMode] = useState(false);
+
     const handleSelectActivity = (id: string) => {
         setSelectedActivity(activities.filter(activity => activity.id === id)[0]);
+        setEditMode(false);
+    };
+
+    const handleOpenCreateForm = () => {
+        setSelectedActivity(null);
+        setEditMode(true);
+    };
+
+    const handleCreateActivity = (activity: IActivity) => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+    };
+
+    const handleEditActivity = (activity: IActivity) => {
+        setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+    };
+
+    const handleDeleteActivity = (id: string) => {
+        setActivities([...activities.filter(a => a.id !== id)]);
     };
 
     useEffect(() => {
         axios.get<IActivity[]>("http://localhost:5000/api/activities").then(response => {
-            setActivities(response.data);
+            let activities: IActivity[] = [];
+            response.data.forEach(activity => {
+                activity.date = activity.date.split(".")[0];
+                activities.push(activity);
+            });
+
+            setActivities(activities);
         });
     }, []);
 
     return (
         <React.Fragment>
-            <NavBar />
+            <NavBar openCreateForm={handleOpenCreateForm} />
             <List>
                 <Container style={{ marginTop: "7em" }}>
                     <ActivityDashboard
                         activities={activities}
                         selectActivity={handleSelectActivity}
                         selectedActivity={selectedActivity}
+                        editMode={editMode}
+                        setEditMode={setEditMode}
+                        setSelectedActivity={setSelectedActivity}
+                        createActivity={handleCreateActivity}
+                        editActivity={handleEditActivity}
+                        deleteActivity={handleDeleteActivity}
                     ></ActivityDashboard>
                 </Container>
             </List>
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Card, Icon, Image, Button } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
     activity: IActivity;
+    setEditMode: (editMode: boolean) => void;
+    setSelectedActivity: (activity: IActivity | null) => void;
 }
 
-const ActivityDetails: React.FC<IProps> = ({ activity }) => {
+const ActivityDetails: React.FC<IProps> = ({ activity, setEditMode, setSelectedActivity }) => {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
@@ -19,8 +21,8 @@ const ActivityDetails: React.FC<IProps> = ({ activity }) => {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths={2}>
-                    <Button basic color="blue" content="Edit" />
-                    <Button basic color="grey" content="Cancel" />
+                    <Button onClick={() => setEditMode(true)} basic color="blue" content="Edit" />
+                    <Button basic onClick={() => setSelectedActivity(null)} color="grey" content="Cancel" />
                 </Button.Group>
             </Card.Content>
         </Card>
